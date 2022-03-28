2314db8e29ac0fe00efab5f36eb877677ba024a3
jyoti patel <jyotipatelfyit77100@gmail.com>
2022-03-28 23:28:20 +0530

    style

--- src/components/ImageAfterUpdoad.js
+++ src/components/ImageAfterUpdoad.js
@@ -1,4 +1,13 @@
 const ImageAfterUpdoad = () => {
-  return <div>ImageAfterUpdoad</div>
+  return (
+    <div className='after-image-contaner'>
+      <img
+        src='https://images-eu.ssl-images-amazon.com/images/I/51T8OXMiB5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+        alt='image'
+        className='img'
+      />
+      <div className='url-contant'></div>
+    </div>
+  )
 }
 export default ImageAfterUpdoad
